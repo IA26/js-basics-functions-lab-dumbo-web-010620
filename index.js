09c3abe2af50a9f8fac1730b7f5cd6f9a3101171
Isaac Avilez <isaac1180@aol.com>
2020-02-18 14:13:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,5 +8,9 @@ function distanceFromHqInBlocks (block) {
 }
 
 function distanceFromHqInFeet(block) {
-  return distanceFromHqInBlocks(block) * 264
+  return distanceFromHqInBlocks(block) * 264;
+}
+
+function distanceTravelledInFeet() {
+  
 }
